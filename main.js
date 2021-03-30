570dccc7493e5aa88a338c8ab38a7474097e2b3a
Lauren Arkell <laarkell@davidson.edu>
2021-03-30 01:40:10 -0400

    changes

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 "use strict";
-
-// const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 
 fetch('https://api.wheretheiss.at/v1/satellites/25544')
   .then(res => res.json())
