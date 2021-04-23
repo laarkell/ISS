f09b7727efad3d40e9cdeb97b89aad480633ee3d
Lauren Arkell <laarkell@davidson.edu>
2021-04-22 22:07:09 -0400

    tabs

--- main.js
+++ main.js
@@ -6,4 +6,24 @@ fetch('https://api.wheretheiss.at/v1/satellites/25544')
   .then(json => {
     console.log(json)
     updateMarker(json.latitude,json.longitude)
-  })
+  });
+
+function changetab1(){
+  $(".facts").load("facts.html")
+}
+function changetab2(){
+  $(".howto").load("howto.html")
+}
+function changetab3(){
+  $(".help").load("help.html")
+}
+
+$(document).on("click", '.howtotab', function() {
+  changetab2();
+});
+$(document).on("click", '.factstab', function() {
+  changetab1();
+});
+$(document).on("click", '.helptab', function() {
+  changetab3();
+});
