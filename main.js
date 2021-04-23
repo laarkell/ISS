720d0c64794a12eb9ef5b45ab92f3eadaad54cca
Owen Mundy <omundy@gmail.com>
2021-04-23 13:30:39 -0400

    Cleaning up with functions, globally accessible variables

--- main.js
+++ main.js
@@ -1,33 +1,27 @@
 "use strict";
-//const fetch = require('node-fetch');
 
-fetch('https://api.wheretheiss.at/v1/satellites/25544')
-  .then(res => res.json())
-  .then(json => {
-    console.log(json)
-    updateMarker(json.latitude,json.longitude)
-  });
+$(document).ready(function() {
+	$(".howto").load("howto.html");
+});
 
-  $( document ).ready(function() {
-      $(".howto").load("howto.html")
-  });
-
-function changetab1(){
-  $(".facts").load("facts.html")
+function changetab1() {
+	$(".facts").load("facts.html");
 }
-function changetab2(){
-  $(".howto").load("howto.html")
+
+function changetab2() {
+	$(".howto").load("howto.html");
 }
-function changetab3(){
-  $(".help").load("help.html")
+
+function changetab3() {
+	$(".help").load("help.html");
 }
 
 $(document).on("click", '.howtotab', function() {
-  changetab2();
+	changetab2();
 });
 $(document).on("click", '.factstab', function() {
-  changetab1();
+	changetab1();
 });
 $(document).on("click", '.helptab', function() {
-  changetab3();
+	changetab3();
 });
