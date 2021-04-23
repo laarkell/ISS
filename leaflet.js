c16ddfca6a612ce745ac2457b29f52b7015063bc
Lauren Arkell <laarkell@davidson.edu>
2021-04-22 22:26:49 -0400

    new lat/lng

--- leaflet.js
+++ leaflet.js
@@ -1,4 +1,17 @@
-var mymap = L.map('mapid').setView([0, 0], 1);
+function newplace(lat,lng){
+  var newLat = lat;
+  var newLng = lng;
+}
+
+fetch('https://api.wheretheiss.at/v1/satellites/25544')
+  .then(res => res.json())
+  .then(json => {
+    console.log(json);
+    newplace(json.latitude,json.longitude);
+  });
+
+
+var mymap = L.map('mapid').setView([newLat, newLng], 3);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
